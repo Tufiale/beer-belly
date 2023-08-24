b3f7fb1899c9694216a7a72755a17b2029d918d9
Tufiale <tufiale@gmail.com>
2023-08-24 11:18:21 +0100

    Added link to beers

--- src/Components/BeerCard/BeerCard.tsx
+++ src/Components/BeerCard/BeerCard.tsx
@@ -19,19 +19,21 @@ const BeerCard = ({
 }: beerCardProps) => {
   return (
     <>
-      <div className="beer-card-container">
-        <div className="beer-card__heading">
-          <img className="beer-card__image" src={image} alt={name} />
-          <h1 className="beer-card__name">{name}</h1>
-          <h3 className="beer-card__tagline">{tagline}</h3>
+      <section>
+        <div className="beer-card-container">
+          <div className="beer-card__heading">
+            <img className="beer-card__image" src={image} alt={name} />
+            <h1 className="beer-card__name">{name}</h1>
+            <h3 className="beer-card__tagline">{tagline}</h3>
+          </div>
+          <div className="beer-card__main">
+            <h4 className="beer-card__description">{description}</h4>
+            <h4 className="beer-card__abv">ABV:{abv}</h4>
+            <h4 className="beer-card__ibu">IBU:{ibu}</h4>
+            <h3>Click here for more information</h3>
+          </div>
         </div>
-        <div className="beer-card__main">
-          <h4 className="beer-card__description">{description}</h4>
-          <h4 className="beer-card__abv">ABV:{abv}</h4>
-          <h4 className="beer-card__ibu">IBU:{ibu}</h4>
-          <h3>Click here for more information</h3>
-        </div>
-      </div>
+      </section>
     </>
   );
 };
