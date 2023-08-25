8750ee98e12d14ed50de077d35a517ad686b202e
Tufiale <tufiale@gmail.com>
2023-08-25 06:55:48 +0100

    Stylings and error page added

--- src/Components/BeerProfile/BeerProfile.tsx
+++ src/Components/BeerProfile/BeerProfile.tsx
@@ -1,5 +1,6 @@
 import { Beer } from "../../Types/Types";
 import { useParams } from "react-router-dom";
+import NoBeers from "../NoBeers/NoBeers";
 import "../BeerProfile/BeerProfile.scss";
 
 type BeerProfileProps = {
@@ -10,28 +11,32 @@ const BeerProfile = ({ beers }: BeerProfileProps) => {
   const { beerId } = useParams();
 
   const beerInfor = beers.find((beer) => beer.id === Number(beerId));
-
-  return (
-    <div className="beer-profile-card">
-      <section className="beer-profile-info">
-        <h1 className="beer-profile__header">{beerInfor?.name}</h1>
-        <img
-          className="beer-profile__image"
-          src={beerInfor?.image_url}
-          alt={beerInfor?.name}
-        />
-        <h2 className="beer-profile__tagline">{beerInfor?.tagline}</h2>
-        <h3>First Brewed:{beerInfor?.first_brewed}</h3>
-        <h3>Description:{beerInfor?.description}</h3>
-        <ul>
-          <li>ABV: {beerInfor?.abv}</li>
-          <li>PH: {beerInfor?.ph}</li>
-          <li>IBU: {beerInfor?.ibu}</li>
-          <li>Attenuation Level: {beerInfor?.attenuation_level}</li>
-        </ul>
-      </section>
-    </div>
-  );
+  if (!beerId) {
+    return <NoBeers />;
+  } else {
+    return (
+      <div className="beer-profile-card">
+        <section className="beer-profile-info">
+          <h1 className="beer-profile__header">{beerInfor?.name}</h1>
+          <img
+            className="beer-profile__image"
+            src={beerInfor?.image_url}
+            alt={beerInfor?.name}
+          />
+          <h2 className="beer-profile__tagline">{beerInfor?.tagline}</h2>
+          <h3>First Brewed:{beerInfor?.first_brewed}</h3>
+          <h3>Description:{beerInfor?.description}</h3>
+          <ul>
+            <li>ABV: {beerInfor?.abv}</li>
+            <li>PH: {beerInfor?.ph}</li>
+            <li>IBU: {beerInfor?.ibu}</li>
+            <li>Attenuation Level: {beerInfor?.attenuation_level}</li>
+          </ul>
+          <h3>Food Pairings: {beerInfor?.food_pairing} </h3>
+        </section>
+      </div>
+    );
+  }
 };
 
 export default BeerProfile;
---
+++ src/Components/NoBeers/NoBeers.tsx
@@ -0,0 +1,12 @@
+import "./NoBeers.scss";
+
+const NoBeers = () => {
+  return (
+    <div className="error-page">
+      <h1 className="error-page__heading">Ah. Can't find that beer.</h1> <br />
+      <h2 className="error-page__heading">Must be time to go Home...</h2>
+    </div>
+  );
+};
+
+export default NoBeers;
