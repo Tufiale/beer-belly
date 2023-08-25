9e4e89943f9b429b3ad0f02adf414ac7529f6eec
Tufiale <tufiale@gmail.com>
2023-08-25 13:18:51 +0100

    live site

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <Link to="/">
+      <Link to="/beer-belly/">
         <h1 className="navbar__header">Beer Belly</h1>
       </Link>
     </nav>
